e3e45c6ef04c584b5abec698b10e3ecbda2a44e5
Jared Hanson <jaredhanson@gmail.com>
2018-03-01 17:32:51 -0800

    Clean up federation initialization.

--- app/initiate/initiate.js
+++ app/initiate/initiate.js
@@ -1,14 +1,9 @@
-exports = module.exports = function(createProtocol, createProvider, idp, initialize, parse, authenticate) {
+exports = module.exports = function(createProtocol, authenticate, idp) {
   
-  function beginFederate(req, res, next) {
-    console.log('BEGIN FEDERATE');
-    console.log(req.query);
-    console.log(req.params);
-    
+  function federate(req, res, next) {
     var provider = req.query.provider;
     idp.resolve(provider, function(err, config) {
-      console.log(err);
-      console.log(config);
+      if (err) { return next(err); }
       
       var protocol = createProtocol(config);
       console.log('PROTOCOL IS:');
@@ -18,33 +13,16 @@ exports = module.exports = function(createProtocol, createProvider, idp, initial
       authenticate(protocol)[1](req, res, next);
       //authenticate(strategy)(req, res, next);
     });
-    
-    return
-    
-    var identifier = req.query.provider;
-    
-    createProvider(identifier, function(err, provider) {
-      if (err) { return next(err); }
-      
-      // TODO: Parse scope and other query options as normalized
-      authenticator.authenticate(provider)(req, res, next);
-    });
   }
 
 
   return [
-    initialize(),
-    parse('application/x-www-form-urlencoded'),
-    beginFederate
+    federate
   ];
 };
 
 exports['@require'] = [
   '../protocol/create',
-  '../createprovider',
+  'http://i.bixbyjs.org/http/middleware/authenticate',
   'http://schemas.authnomicon.org/js/federation/idp',
-  'http://i.bixbyjs.org/http/middleware/initialize',
-  'http://i.bixbyjs.org/http/middleware/parse',
-  'http://i.bixbyjs.org/http/middleware/authenticate'
-  //'http://i.bixbyjs.org/http/Authenticator'
 ];
