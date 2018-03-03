68043a7a90a2d9127fa8fab3a76fa95ef2155f05
Jared Hanson <jaredhanson@gmail.com>
2018-03-03 08:59:47 -0800

    Remove createprovider component.

--- app/createprovider.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = function(IoC) {
-  // TODO: Introspect the container for someething implementing interface, and return
-  //       if possible, otherwise fallback to internals.
-  
-  return IoC.create('./internals/createprovider');
-};
-
-exports['@singleton'] = true;
-exports['@require'] = [ '!container' ];
