63119d1d556e737a080f97cea43402a1edd27eaf
Jared Hanson <jaredhanson@gmail.com>
2018-04-07 12:19:00 -0700

    Expose login service.

--- test/package.test.js
+++ test/package.test.js
@@ -11,8 +11,9 @@ describe('@authnomicon/federation', function() {
     it('should have assembly metadata', function() {
       expect(json.assembly.namespace).to.equal('org.authnomicon/federation');
       
-      expect(json.assembly.components).to.have.length(5);
+      expect(json.assembly.components).to.have.length(6);
       expect(json.assembly.components).to.include('http/initiate/service');
+      expect(json.assembly.components).to.include('http/login/service');
       expect(json.assembly.components).to.include('http/oauth/service');
       expect(json.assembly.components).to.include('http/oauth2/service');
       expect(json.assembly.components).to.include('http/oauth2/yielders/login');
