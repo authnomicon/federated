067615f0804c44123daec2f4c87147b6afeee7b9
Jared Hanson <jaredhanson@gmail.com>
2018-05-15 15:41:11 -0700

    Add test case.

--- test/http/handlers/initiate.test.js
+++ test/http/handlers/initiate.test.js
@@ -17,17 +17,25 @@ describe('http/handlers/initiate', function() {
     expect(factory['@singleton']).to.be.undefined;
   });
   
-  describe.skip('handler', function() {
+  describe('handler', function() {
     var idp = {
       resolve: function(){}
+    }
+    
+    function authenticate(protocol, options) {
+      return [ null, function(req, res, next) {
+        res.redirect('https://id.example.com/authorize');
+      } ];
     }
     
     
     describe('initiating sso', function() {
       var request, response;
+      var createProtocolStub = sinon.stub().returns('openid-connect')
+        , authenticateSpy = sinon.spy(authenticate)
       
       before(function() {
-        sinon.stub(idp, 'resolve').yields(null, { id: '1234-5678', authorizeURL: 'https://id.example.com/authorize' });
+        sinon.stub(idp, 'resolve').yields(null, { identifier: 'https://id.example.com' });
       });
       
       after(function() {
@@ -35,7 +43,7 @@ describe('http/handlers/initiate', function() {
       });
       
       before(function(done) {
-        var handler = factory(null, idp);
+        var handler = factory(createProtocolStub, idp, authenticateSpy);
         
         chai.express.handler(handler)
           .req(function(req) {
@@ -53,21 +61,32 @@ describe('http/handlers/initiate', function() {
           .dispatch();
       });
       
-      it('should set state', function() {
-        expect(request.state).to.deep.equal({
-          failureCount: 1
+      it('should call idp.resolve', function() {
+        expect(idp.resolve.callCount).to.equal(1);
+        expect(idp.resolve.args[0][0]).to.equal('https://id.example.com');
+      });
+      
+      it('should call createProtocol', function() {
+        expect(createProtocolStub.callCount).to.equal(1);
+        expect(createProtocolStub.args[0][0]).to.deep.equal({ identifier: 'https://id.example.com' });
+      });
+      
+      it('should call authenticate', function() {
+        expect(authenticateSpy.callCount).to.equal(1);
+        expect(authenticateSpy.args[0][0]).to.equal('openid-connect');
+        expect(authenticateSpy.args[0][1]).to.deep.equal({
+          state: {
+            provider: {
+              identifier: 'https://id.example.com'
+            },
+            state: undefined
+          }
         });
       });
       
-      it('should set locals', function() {
-        expect(response.locals).to.deep.equal({
-          message: 'Invalid username or password',
-          failureCount: 1
-        });
-      });
-      
-      it('should prompt', function() {
-        expect(response.statusCode).to.equal(200);
+      it('should redirect', function() {
+        expect(response.statusCode).to.equal(302);
+        expect(response.getHeader('Location')).to.equal('https://id.example.com/authorize');
       });
     }); // initiating sso
     
