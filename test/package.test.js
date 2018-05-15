b6663f54c67054a6f013c8024fe4d3cc8c3b8033
Jared Hanson <jaredhanson@gmail.com>
2018-05-15 12:11:36 -0700

    Fix tests.

--- test/package.test.js
+++ test/package.test.js
@@ -9,7 +9,7 @@ describe('@authnomicon/federation', function() {
     var json = require('../package.json');
     
     it('should have assembly metadata', function() {
-      expect(json.assembly.namespace).to.equal('org.authnomicon/federation');
+      expect(json.assembly.namespace).to.equal('org.authnomicon/sso');
       
       expect(json.assembly.components).to.have.length(6);
       expect(json.assembly.components).to.include('http/initiate/service');
