fcd2a6d1bac2ba13eb4d00ed652ba03eec793a1c
Jared Hanson <jaredhanson@gmail.com>
2019-10-12 14:47:28 -0700

    Only provide HTTP service.

--- app/http/service.js
+++ app/http/service.js
@@ -7,9 +7,6 @@ exports = module.exports = function(initiateHandler) {
   return router;
 };
 
-exports['@implements'] = [
-  'http://i.bixbyjs.org/http/Service',
-  'http://schemas.authnomicon.org/js/http/sso/InitiationService'
-];
+exports['@implements'] = 'http://i.bixbyjs.org/http/Service';
 exports['@path'] = '/federate';
 exports['@require'] = [ './handlers/initiate' ];
