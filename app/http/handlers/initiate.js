48ac45e025342750368956a43e9e7d82dccc2e1a
Jared Hanson <jaredhanson@gmail.com>
2019-10-12 17:42:55 -0700

    Remove dead code.

--- app/http/handlers/initiate.js
+++ app/http/handlers/initiate.js
@@ -2,14 +2,10 @@ exports = module.exports = function(IDPFactory, authenticate) {
   
   function federate(req, res, next) {
     var provider = req.query.provider;
-    console.log(req.query);
     
+    // TODO: Past `host` as option
     IDPFactory.create(provider)
       .then(function(idp) {
-        console.log('GOT IDP!');
-        console.log(idp);
-        
-        
         var options = {};
         options.state = {
           //provider: config,
@@ -29,28 +25,6 @@ exports = module.exports = function(IDPFactory, authenticate) {
         
         next(err);
       });
-    
-    return;
-    
-    // TODO: Past `host` as option
-    idp.resolve(provider, function(err, config) {
-      if (err) { return next(err); }
-      
-      var protocol = createProtocol(config);
-      
-      var options = {};
-      options.state = {
-        provider: config,
-        state: req.query.state
-      }
-      // TODO: Add a `context` option, used to pass the database ID of this IdP,
-      //       which can be serialized into the state for faster resumption when
-      //       the IdP redirects back.
-      
-      // FIXME: Remove the array index here, once passport.initialize is no longer needed
-      authenticate(protocol, options)[1](req, res, next);
-      //authenticate(strategy)(req, res, next);
-    });
   }
 
 
