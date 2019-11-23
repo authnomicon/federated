81010a6479d9ff282757cbc5622e0ea58200005f
Jared Hanson <jaredhanson@gmail.com>
2019-11-23 09:54:12 -0800

    Clean up initiate handler.

--- app/http/handlers/initiate.js
+++ app/http/handlers/initiate.js
@@ -6,17 +6,21 @@ exports = module.exports = function(IDPFactory, authenticate) {
     // TODO: Past `host` as option
     IDPFactory.create(provider)
       .then(function(idp) {
-        var options = {};
-        options.state = {
+        var state = {
           provider: provider,
-          returnTo: req.header('referer'),
-          // FIXME: rename key to "state"
-          parent: req.query.state
+          returnTo: req.header('referer')
         }
+        
+        // FIXME: rename "parent" to "state"
+        if (req.query.state) { state.parent = req.query.state; }
         
         // TODO: Add a `context` option, used to pass the database ID of this IdP,
         //       which can be serialized into the state for faster resumption when
         //       the IdP redirects back.
+        
+        var options = {
+          state: state
+        };
         
         // FIXME: Remove the array index here, once passport.initialize is no longer needed
         authenticate(idp, options)[1](req, res, next);
