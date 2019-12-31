d30acd41c742bb0466e15e14a2726f5fd57dcf06
Jared Hanson <jaredhanson@gmail.com>
2019-12-31 12:24:26 -0800

    Remove dead code, add tests.

--- app/http/oauth/handlers/callback.js
+++ app/http/oauth/handlers/callback.js
@@ -29,45 +29,8 @@ exports = module.exports = function(IDPFactory, /*idp,*/ authenticate, ceremony)
       return next();
     });
   }
-
-
-  function old_federate(req, res, next) {
-    var provider = req.state.provider;
-    // TODO: Past `host` as option
-    // TODO: Pass `idpID` as option, if available in state
-    // TODO: Pass `clientID` as option, if available
-    idp.resolve(provider, function(err, config) {
-      if (err) { return next(err); }
-      
-      var protocol = createProtocol(config);
-      // FIXME: Remove the array index here, once passport.initialize is no longer needed
-      authenticate(protocol, { assignProperty: 'federatedUser' })[1](req, res, next);
-    });
-  }
-
-  function postProcess(req, res, next) {
-    // Fake provision a user
-    var user = { id: '5001' };
-    user.displayName = 'Federated ' + req.federatedUser.displayName;
-    
-    req.user = user;
-    next();
-    
-    return;
-    
-    // TODO Abstract this out into something common, shared between protocls
-    // https://en.wikipedia.org/wiki/Federated_identity
-    
-    if (!req.user) {
-      // TODO: Local account provisioning w/ initial linked account
-    } else {
-      // TODO: Account linking, storing access and refresh tokens, etc.
-    }
-    next();
-  }
-
-
-  //return ceremony('oauth/callback',
+  
+  
   return ceremony(
     federate,
     [ establishSession ],
