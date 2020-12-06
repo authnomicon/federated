0b4df6ebbc3f4c066431348a05793aa428cf1b4c
Jared Hanson <jaredhanson@gmail.com>
2020-12-05 21:10:08 -0800

    Add tests.

--- test/http/service.test.js
+++ test/http/service.test.js
@@ -13,8 +13,17 @@ describe('http/service', function() {
   
   it('should be annotated', function() {
     expect(factory['@implements']).to.deep.equal('http://i.bixbyjs.org/http/Service');
-    expect(factory['@path']).to.equal('/federate');
+    expect(factory['@path']).to.equal('/login/federated');
     expect(factory['@singleton']).to.be.undefined;
+  });
+  
+  it('should create service', function() {
+    function initiateHandler() {};
+  
+    var service = factory(initiateHandler);
+    
+    expect(service).to.be.a('function');
+    expect(service.length).to.equal(3);
   });
   
 });
