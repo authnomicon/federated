906473b83b02b9edbad00b08cdb3b97cc3fa2edd
Jared Hanson <jaredhanson@gmail.com>
2020-12-11 21:40:34 -0800

    Fix package test.

--- test/package.test.js
+++ test/package.test.js
@@ -15,7 +15,7 @@ describe('@authnomicon/sso', function() {
     var json = require('../package.json');
     
     it('should have assembly metadata', function() {
-      expect(json.assembly.namespace).to.equal('org.authnomicon/federate');
+      expect(json.assembly.namespace).to.equal('org.authnomicon/federated');
       
       expect(json.assembly.components).to.have.length(7);
       expect(json.assembly.components).to.include('http/service');
