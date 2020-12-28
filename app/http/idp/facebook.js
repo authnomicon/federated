0657889909113ede927d3a478cec461119ef4469
Jared Hanson <jaredhanson@gmail.com>
2020-12-28 11:45:30 -0800

    Add proxyquire devDep, update @requires.

--- app/http/idp/facebook.js
+++ app/http/idp/facebook.js
@@ -6,7 +6,7 @@ exports = module.exports = function(store, keyring) {
     
     keyring.get('graph.facebook.com', function(err, cred) {
       if (err) { return reject(err); }
-    
+      
       var strategy = new FacebookStrategy({
           clientID: cred.username,
           clientSecret: cred.password,
@@ -26,6 +26,6 @@ exports = module.exports = function(store, keyring) {
 exports['@implements'] = 'http://i.authnomicon.org/sso/http/IDProvider';
 exports['@provider'] = 'https://www.facebook.com';
 exports['@require'] = [
-  'http://i.authnomicon.org/sso/oauth2/http/StateStore',
+  'http://i.authnomicon.org/federated/oauth2/http/StateStore',
   'http://i.bixbyjs.org/security/Keyring'
 ];
