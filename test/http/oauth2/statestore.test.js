71cc5dfb4e10162c5c58136b2bf0ea2869987586
Jared Hanson <jaredhanson@gmail.com>
2020-12-27 16:20:42 -0800

    Add tests for state store.

--- test/http/oauth2/statestore.test.js
+++ test/http/oauth2/statestore.test.js
@@ -1,8 +1,10 @@
 /* global describe, it, expect */
 
+var $require = require('proxyquire');
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var factory = require('../../../app/http/oauth2/statestore');
+var StateStore = require('../../../lib/oauth2/statestore');
 
 
 describe('http/oauth2/statestore', function() {
@@ -12,8 +14,25 @@ describe('http/oauth2/statestore', function() {
   });
   
   it('should be annotated', function() {
-    expect(factory['@implements']).to.equal('http://i.authnomicon.org/sso/oauth2/http/StateStore');
+    expect(factory['@implements']).to.equal('http://i.authnomicon.org/federated/oauth2/http/StateStore');
     expect(factory['@singleton']).to.equal(true);
+  });
+  
+  describe('creating with defaults', function() {
+    var StateStoreSpy = sinon.spy(StateStore);
+    
+    var factory = $require('../../../app/http/oauth2/statestore',
+      { '../../../lib/oauth2/statestore': StateStoreSpy });
+    var store = factory();
+    
+    it('should construct store', function() {
+      expect(StateStoreSpy).to.have.been.calledOnce;
+      expect(StateStoreSpy).to.have.been.calledWithNew;
+    });
+  
+    it('should return store', function() {
+      expect(store).to.be.an.instanceOf(StateStore);
+    });
   });
   
 });
