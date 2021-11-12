c35c61c4660c5a8b2de257d13a392b64c5f2e552
Jared Hanson <jaredhanson@gmail.com>
2021-11-12 10:18:02 -0800

    Clean up tests.

--- test/http/handlers/initiate.test.js
+++ test/http/handlers/initiate.test.js
@@ -50,7 +50,7 @@ describe('http/handlers/initiate', function() {
   
   describe('handler', function() {
     
-    it('should create IdP from provider and authenticate', function(done) {
+    it('should authenticate with provider', function(done) {
       var idp = new Object();
       var idpFactory = new Object();
       idpFactory.create = sinon.stub().resolves(idp)
@@ -81,81 +81,41 @@ describe('http/handlers/initiate', function() {
         })
         .next(done)
         .listen();
-    }); // should create IdP from provider and authenticate
+    }); // should authenticate with provider
     
-    describe('federating with provider and protocol', function() {
+    it('should authenticate with provider and protocol', function(done) {
       var idp = new Object();
       var idpFactory = new Object();
-      idpFactory.create = sinon.stub().resolves(idp)
-      
-      function authenticate(idp, options) {
-        return function(req, res, next) {
-          res.redirect('https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient.example.com%2Fcb');
-        };
-      }
-      
-      function state() {
-        return function(req, res, next) {
-          next();
-        };
-      }
-      
-      function session() {
-        return function(req, res, next) {
-          next();
-        };
-      }
-      
+      idpFactory.create = sinon.stub().resolves(idp);
       var authenticateSpy = sinon.spy(authenticate);
-      var stateSpy = sinon.spy(state);
-      var sessionSpy = sinon.spy(session);
       
-      
-      var request, response;
-      
-      before(function(done) {
-        var handler = factory(idpFactory, authenticateSpy, stateSpy, sessionSpy);
+      var handler = factory(idpFactory, authenticateSpy, state, session);
         
-        chai.express.use(handler)
-          .request(function(req, res) {
-            request = req;
-            req.query = {
-              provider: 'https://server.example.com',
-              protocol: 'oauth2'
-            };
-            
-            response = res;
-          })
-          .finish(function() {
-            done();
-          })
-          .listen();
-      });
-      
-      it('should setup middleware', function() {
-        expect(sessionSpy).to.be.calledOnce;
-        expect(stateSpy).to.be.calledOnce;
-      });
-      
-      it('should create identity provider', function() {
-        expect(idpFactory.create).to.be.calledOnce;
-        expect(idpFactory.create).to.be.calledWithExactly('https://server.example.com', 'oauth2', {});
-      });
-      
-      it('should authenticate with identity provider', function() {
-        expect(authenticateSpy).to.be.calledOnce;
-        expect(authenticateSpy).to.be.calledWithExactly(idp, {
-          state: {
-            provider: 'https://server.example.com'
-          }
-        });
-      });
-      
-      it('should redirect', function() {
-        expect(response.statusCode).to.equal(302);
-        expect(response.getHeader('Location')).to.equal('https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient.example.com%2Fcb');
-      });
-    }); // federating with provider and protocol
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.query = {
+            provider: 'https://server.example.com',
+            protocol: 'oauth2'
+          };
+        })
+        .finish(function() {
+          expect(idpFactory.create).to.be.calledOnce;
+          expect(idpFactory.create).to.be.calledWithExactly('https://server.example.com', 'oauth2', {});
+          
+          expect(authenticateSpy).to.be.calledOnce;
+          expect(authenticateSpy).to.be.calledWithExactly(idp, {
+            state: {
+              provider: 'https://server.example.com'
+            }
+          });
+          
+          expect(this.statusCode).to.equal(302);
+          expect(this.getHeader('Location')).to.equal('https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient.example.com%2Fcb');
+          done();
+        })
+        .next(done)
+        .listen();
+    }); // should authenticate with provider and protocol
     
     describe('federating with provider from state', function() {
       var idp = new Object();
