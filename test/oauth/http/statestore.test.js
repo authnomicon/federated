e1e42fd0ad43e104ead96d8806a90fc6fa6a3c9f
Jared Hanson <jaredhanson@gmail.com>
2021-11-11 20:10:32 -0800

    Clean up tests.

--- test/oauth/http/statestore.test.js
+++ test/oauth/http/statestore.test.js
@@ -1,48 +1,36 @@
 /* global describe, it, expect */
 
-var $require = require('proxyquire');
 var expect = require('chai').expect;
+var $require = require('proxyquire');
 var sinon = require('sinon');
 var factory = require('../../../com/oauth/http/statestore');
-var StateStore = require('../../../lib/oauth/statestore');
+var RequestTokenStore = require('../../../lib/oauth/statestore');
 
 
 describe('oauth/http/requesttokenstore', function() {
-  
-  it('should export factory function', function() {
-    expect(factory).to.be.a('function');
-  });
   
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal('http://i.authnomicon.org/federated/oauth/http/RequestTokenStore');
     expect(factory['@singleton']).to.equal(true);
   });
   
-  describe('creating with defaults', function() {
-    var StateStoreSpy = sinon.spy(StateStore);
-    var factory = $require('../../../com/oauth/http/statestore',
-      { '../../../lib/oauth/statestore': StateStoreSpy });
-    
-    var stateStore = new Object();
-    var store = factory(stateStore);
-    
-    // FIXME: Re-enable this test
-    it.skip('should construct store', function() {
-      expect(StateStoreSpy).to.have.been.calledOnce;
-      expect(StateStoreSpy).to.have.been.calledWithNew;
-      expect(StateStoreSpy).to.have.been.calledWith(stateStore);
+  it('should construct RequestTokenStore', function() {
+    var RequestTokenStoreSpy = sinon.spy(RequestTokenStore);
+    var factory = $require('../../../com/oauth/http/statestore', {
+      '../../../lib/oauth/statestore': RequestTokenStoreSpy
     });
+    
+    var store = factory();
+    expect(RequestTokenStoreSpy).to.have.been.calledOnce;
+    expect(RequestTokenStoreSpy).to.have.been.calledWithNew;
+    expect(store).to.be.an.instanceOf(RequestTokenStore);
+  });
   
-    it('should return store', function() {
-      expect(store).to.be.an.instanceOf(StateStore);
-    });
-  }); // creating with defaults
   
-  
-  describe('StateStore', function() {
+  describe('RequestTokenStore', function() {
     
     describe('#get', function() {
-      var store = new StateStore();
+      var store = new RequestTokenStore();
       
       describe('getting token secret', function() {
         var req = new Object();
@@ -108,7 +96,7 @@ describe('oauth/http/requesttokenstore', function() {
           })
         });
         
-        var store = new StateStore(_store);
+        var store = new RequestTokenStore(_store);
         
         var req = new Object();
         req.state = new Object();
@@ -151,7 +139,7 @@ describe('oauth/http/requesttokenstore', function() {
           })
         });
         
-        var store = new StateStore(_store);
+        var store = new RequestTokenStore(_store);
         
         var req = new Object();
         req.state = new Object();
@@ -195,7 +183,7 @@ describe('oauth/http/requesttokenstore', function() {
           })
         });
         
-        var store = new StateStore(_store);
+        var store = new RequestTokenStore(_store);
         
         var req = new Object();
         req.state = new Object();
