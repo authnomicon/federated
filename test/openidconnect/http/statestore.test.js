d8706c24e50ff629c8780a52a3eb1d7854daa59c
Jared Hanson <jaredhanson@gmail.com>
2021-11-12 06:39:22 -0800

    Add tests.

--- test/openidconnect/http/statestore.test.js
+++ test/openidconnect/http/statestore.test.js
@@ -26,4 +26,36 @@ describe('openidconnect/http/statestore', function() {
     expect(store).to.be.an.instanceOf(StateStore);
   });
   
+  
+  describe('StateStore', function() {
+    var store = new StateStore();
+    
+    it('should push state and yield handle', function(done) {
+      var req = new Object();
+      req.state = new Object();
+      req.pushState = sinon.stub().yieldsAsync(null, 'af0ifjsldkj');
+      
+      var ctx = {};
+      var state = { provider: 'https://server.example.com' };
+      var meta = {
+        authorizationURL: 'https://server.example.com/authorize',
+        tokenURL: 'https://server.example.com/token',
+        clientID: 's6BhdRkqt3',
+        callbackURL: 'https://client.example.com/cb'
+      }
+      
+      store.store(req, ctx, state, meta, function(err, handle) {
+        if (err) { return done(err); }
+        
+        expect(req.pushState).to.have.been.calledOnceWith({
+          provider: 'https://server.example.com'
+        }, 'https://client.example.com/cb');
+        
+        expect(handle).to.equal('af0ifjsldkj');
+        done();
+      });
+    }); // should push state and yield handle
+    
+  });
+  
 });
