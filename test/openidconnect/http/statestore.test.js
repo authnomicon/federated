7bc3be6a72a8b24eb0615026ef3e8dbdb153e6e2
Jared Hanson <jaredhanson@gmail.com>
2021-11-12 07:10:57 -0800

    Add tests.

--- test/openidconnect/http/statestore.test.js
+++ test/openidconnect/http/statestore.test.js
@@ -100,16 +100,51 @@ describe('openidconnect/http/statestore', function() {
         req.state = new Object();
         req.state.complete = sinon.spy();
         
-        store.verify(req, 'af0ifjsldkj', function(err, ok, state) {
+        store.verify(req, 'af0ifjsldkj', function(err, ctx, state) {
           if (err) { return done(err); }
           
           expect(req.state.complete).to.have.been.calledOnce;
           
-          expect(ok).to.deep.equal({});
+          expect(ctx).to.deep.equal({});
           expect(state).to.be.undefined;
           done();
         });
       }); // should verify state
+      
+      it('should verify state with nonce', function(done) {
+        var req = new Object();
+        req.query = {
+          code: 'SplxlOBeZQQYbYS6WxSbIA',
+          state: 'af0ifjsldkj'
+        };
+        req.state = new Object();
+        req.state.nonce = 'n-0S6_WzA2Mj';
+        req.state.complete = sinon.spy();
+        
+        store.verify(req, 'af0ifjsldkj', function(err, ctx, state) {
+          if (err) { return done(err); }
+          
+          expect(req.state.complete).to.have.been.calledOnce;
+          
+          expect(ctx).to.deep.equal({
+            nonce: 'n-0S6_WzA2Mj'
+          });
+          expect(state).to.be.undefined;
+          done();
+        });
+      }); // should verify state with nonce
+      
+      it('should error when state middleware is not in use', function(done) {
+        var req = new Object();
+      
+        store.verify(req, 'af0ifjsldkj', function(err, ctx, info) {
+          expect(err).to.be.an.instanceOf(Error);
+          expect(err.message).to.equal('OpenID Connect requires state support. Did you forget to use `flowstate` middleware?');
+          expect(ctx).to.be.undefined;
+          expect(info).to.be.undefined;
+          done();
+        });
+      }); // should error when state middleware is not in use
       
     }); // #verify
     
