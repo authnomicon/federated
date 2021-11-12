5bc1a1367523fd583ef4809d73edc849adf5531c
Jared Hanson <jaredhanson@gmail.com>
2021-11-12 13:17:37 -0800

    Clean up tests.

--- test/http/handlers/initiate.test.js
+++ test/http/handlers/initiate.test.js
@@ -189,81 +189,47 @@ describe('http/handlers/initiate', function() {
         .listen();
     }); // should authenticate with provider and protocol from state
     
-    describe('federating with provider and parameters in state', function() {
-      var idp = new Object();
-      var idpFactory = new Object();
-      idpFactory.create = sinon.stub().resolves(idp)
-      
+    it('should authenticate with parameters from state', function(done) {
       function authenticate(idp, options) {
         return function(req, res, next) {
           res.redirect('https://example.myshopify.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient.example.com%2Fcb');
         };
       }
       
-      function state() {
-        return function(req, res, next) {
+      var idp = new Object();
+      var idpFactory = new Object();
+      idpFactory.create = sinon.stub().resolves(idp)
+      var authenticateSpy = sinon.spy(authenticate);
+      
+      var handler = factory(idpFactory, authenticateSpy, state, session);
+      
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.query = {};
           req.state = new Object();
           req.state.provider = 'https://myshopify.com';
           req.state.shop = 'example';
-          next();
-        };
-      }
-      
-      function session() {
-        return function(req, res, next) {
-          next();
-        };
-      }
-      
-      var authenticateSpy = sinon.spy(authenticate);
-      var stateSpy = sinon.spy(state);
-      var sessionSpy = sinon.spy(session);
-      
-      
-      var request, response;
-      
-      before(function(done) {
-        var handler = factory(idpFactory, authenticateSpy, stateSpy, sessionSpy);
-        
-        chai.express.use(handler)
-          .request(function(req, res) {
-            request = req;
-            req.query = {};
-            
-            response = res;
-          })
-          .finish(function() {
-            done();
-          })
-          .listen();
-      });
-      
-      it('should setup middleware', function() {
-        expect(sessionSpy).to.be.calledOnce;
-        expect(stateSpy).to.be.calledOnce;
-      });
-      
-      it('should create identity provider', function() {
-        expect(idpFactory.create).to.be.calledOnce;
-        expect(idpFactory.create).to.be.calledWithExactly('https://myshopify.com', undefined, { shop: 'example' });
-      });
-      
-      it('should authenticate with identity provider', function() {
-        expect(authenticateSpy).to.be.calledOnce;
-        expect(authenticateSpy).to.be.calledWithExactly(idp, {
-          state: {
-            provider: 'https://myshopify.com',
-            shop: 'example'
-          }
-        });
-      });
-      
-      it('should redirect', function() {
-        expect(response.statusCode).to.equal(302);
-        expect(response.getHeader('Location')).to.equal('https://example.myshopify.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient.example.com%2Fcb');
-      });
-    }); // federating with provider and parameters in state
+        })
+        .finish(function() {
+          expect(idpFactory.create).to.be.calledOnce;
+          expect(idpFactory.create).to.be.calledWithExactly('https://myshopify.com', undefined, { shop: 'example' });
+          
+          expect(authenticateSpy).to.be.calledOnce;
+          expect(authenticateSpy).to.be.calledWithExactly(idp, {
+            state: {
+              provider: 'https://myshopify.com',
+              shop: 'example'
+            }
+          });
+          
+          expect(this.statusCode).to.equal(302);
+          expect(this.getHeader('Location')).to.equal('https://example.myshopify.com/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=https%3A%2F%2Fclient.example.com%2Fcb');
+          done();
+        })
+        .next(done)
+        .listen();
+    }); // should authenticate with parameters from state
     
-  });
+  }); // handler
   
 });
