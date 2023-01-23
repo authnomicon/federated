b43124a83ab98219834137ea48a2b7a5bd520eed
Jared Hanson <jaredhanson@gmail.com>
2023-01-23 07:14:35 -0800

    Move Facebook and Twitter to attic.

--- test/package.test.js
+++ test/package.test.js
@@ -12,14 +12,12 @@ describe('@authnomicon/federated', function() {
     it('should have assembly metadata', function() {
       expect(json.assembly.namespace).to.equal('org.authnomicon/federated');
       
-      expect(json.assembly.components).to.have.length(9);
+      expect(json.assembly.components).to.have.length(7);
       expect(json.assembly.components).to.include('http/service');
       expect(json.assembly.components).to.include('oauth/http/service');
       expect(json.assembly.components).to.include('oauth/http/requesttokenstore');
       expect(json.assembly.components).to.include('oauth2/http/service');
       expect(json.assembly.components).to.include('oauth2/http/statestore');
-      expect(json.assembly.components).to.include('idp/http/facebook');
-      expect(json.assembly.components).to.include('idp/http/twitter');
     });
   });
   
