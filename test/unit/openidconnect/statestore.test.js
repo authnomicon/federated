2732da5da57cde6ddfb6ae5c0cc1ac9f08c90cc9
Jared Hanson <jaredhanson@gmail.com>
2023-01-28 07:11:22 -0800

    Clean up tests.

--- test/unit/openidconnect/statestore.test.js
+++ test/unit/openidconnect/statestore.test.js
@@ -40,6 +40,61 @@ describe('openidconnect/StateStore', function() {
       });
     }); // should push state and yield handle
     
-  });
+    it('should push state with nonce and yield handle', function(done) {
+      var req = new Object();
+      req.pushState = sinon.stub().yieldsAsync(null, 'af0ifjsldkj');
+    
+      var ctx = { nonce: 'n-0S6_WzA2Mj' };
+      var state = { provider: 'https://server.example.com' };
+      var meta = {
+        issuer: 'https://server.example.com',
+        authorizationURL: 'https://server.example.com/authorize',
+        tokenURL: 'https://server.example.com/token',
+        clientID: 's6BhdRkqt3',
+        callbackURL: 'https://client.example.com/cb'
+      }
+    
+      store.store(req, ctx, state, meta, function(err, handle) {
+        if (err) { return done(err); }
+      
+        expect(req.pushState).to.have.been.calledOnceWith({
+          provider: 'https://server.example.com',
+          protocol: 'openidconnect',
+          nonce: 'n-0S6_WzA2Mj'
+        }, 'https://client.example.com/cb');
+      
+        expect(handle).to.equal('af0ifjsldkj');
+        done();
+      });
+    }); // should push state with nonce and yield handle
+    
+    it('should yeild error when pushing state fails', function(done) {
+      var req = new Object();
+      req.pushState = sinon.stub().yieldsAsync(new Error('something went wrong'));
+    
+      var ctx = {};
+      var state = { provider: 'https://server.example.com' };
+      var meta = {
+        issuer: 'https://server.example.com',
+        authorizationURL: 'https://server.example.com/authorize',
+        tokenURL: 'https://server.example.com/token',
+        clientID: 's6BhdRkqt3',
+        callbackURL: 'https://client.example.com/cb'
+      }
+    
+      store.store(req, ctx, state, meta, function(err, handle) {
+        expect(req.pushState).to.have.been.calledOnceWith({
+          provider: 'https://server.example.com',
+          protocol: 'openidconnect'
+        }, 'https://client.example.com/cb');
+        
+        expect(err).to.be.an.instanceOf(Error);
+        expect(err.message).to.equal('something went wrong');
+        expect(handle).to.be.undefined;
+        done();
+      });
+    }); // should yeild error when pushing state fails
+    
+  }); // #store
   
 });
