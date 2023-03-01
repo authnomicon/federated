a73da146cf3ba3537203a88f81fe8f4f336e9337
Jared Hanson <jaredhanson@gmail.com>
2023-03-01 09:38:49 -0800

    Add tests.

--- test/oauth/http/handlers/callback.test.js
+++ test/oauth/http/handlers/callback.test.js
@@ -85,6 +85,167 @@ describe('oauth/http/handlers/callback', function() {
         .listen();
     }); // should federate with provider
     
+    it('should federate with provider and resume', function(done) {
+      var actions = new Object();
+      actions.dispatch = sinon.stub().yieldsAsync(null);
+      var idp = new Object();
+      var idpFactory = new Object();
+      idpFactory.create = sinon.stub().resolves(idp);
+      var authenticateSpy = sinon.spy(authenticate);
+      var store = new Object();
+      store.get = sinon.stub().yieldsAsync(null, {
+        location: 'https://www.example.com/oauth/callback',
+        provider: 'http://sp.example.com',
+        returnTo: '/app'
+      });
+      store.destroy = sinon.stub().yieldsAsync();
+      
+      
+      var handler = factory(actions, idpFactory, { authenticate: authenticateSpy }, store);
+    
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.connection = { encrypted: true };
+          req.method = 'GET';
+          req.url = '/oauth/callback';
+          req.headers.host = 'www.example.com';
+          req.params = { hostname: 'twitter.com' };
+          req.query = { oauth_token: 'XXXXXXXX' };
+          req.session = {};
+          req.session.state = {};
+          req.session.state['oauth_twitter.com_XXXXXXXX'] = { provider: 'http://sp.example.com' };
+        
+          res.resumeState = sinon.spy(function(cb) {
+            if (request.state.returnTo) {
+              return this.redirect(request.state.returnTo);
+            }
+          
+            process.nextTick(cb);
+          });
+        })
+        .finish(function() {
+          expect(store.get).to.be.calledOnceWith(this.req, 'oauth_twitter.com_XXXXXXXX');
+          expect(idpFactory.create).to.be.calledOnceWithExactly('http://sp.example.com', 'oauth');
+          expect(authenticateSpy).to.be.calledOnceWithExactly(idp, { assignProperty: 'federatedUser' });
+          expect(actions.dispatch).to.be.calledOnceWith('login');
+          expect(store.destroy).to.be.calledOnceWith(this.req, 'oauth_twitter.com_XXXXXXXX');
+          
+          expect(this.statusCode).to.equal(302);
+          expect(this.getHeader('Location')).to.equal('/app');
+          
+          done();
+        })
+        .listen();
+    }); // should federate with provider and resume
+    
+    it('should federate with provider and execute action', function(done) {
+      var actions = new Object();
+      actions.dispatch = sinon.stub().yieldsAsync(null);
+      var idp = new Object();
+      var idpFactory = new Object();
+      idpFactory.create = sinon.stub().resolves(idp);
+      var authenticateSpy = sinon.spy(authenticate);
+      var store = new Object();
+      store.get = sinon.stub().yieldsAsync(null, {
+        location: 'https://www.example.com/oauth/callback',
+        provider: 'http://sp.example.com',
+        action: 'authorize'
+      });
+      store.destroy = sinon.stub().yieldsAsync();
+      
+      
+      var handler = factory(actions, idpFactory, { authenticate: authenticateSpy }, store);
+    
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.connection = { encrypted: true };
+          req.method = 'GET';
+          req.url = '/oauth/callback';
+          req.headers.host = 'www.example.com';
+          req.params = { hostname: 'twitter.com' };
+          req.query = { oauth_token: 'XXXXXXXX' };
+          req.session = {};
+          req.session.state = {};
+          req.session.state['oauth_twitter.com_XXXXXXXX'] = { provider: 'http://sp.example.com' };
+        
+          res.resumeState = sinon.spy(function(cb) {
+            if (request.state.returnTo) {
+              return this.redirect(request.state.returnTo);
+            }
+          
+            process.nextTick(cb);
+          });
+        })
+        .finish(function() {
+          expect(store.get).to.be.calledOnceWith(this.req, 'oauth_twitter.com_XXXXXXXX');
+          expect(idpFactory.create).to.be.calledOnceWithExactly('http://sp.example.com', 'oauth');
+          expect(authenticateSpy).to.be.calledOnceWithExactly(idp, { assignProperty: 'federatedUser' });
+          expect(actions.dispatch).to.be.calledOnceWith('authorize');
+          expect(store.destroy).to.be.calledOnceWith(this.req, 'oauth_twitter.com_XXXXXXXX');
+          
+          expect(this.statusCode).to.equal(302);
+          expect(this.getHeader('Location')).to.equal('/');
+          
+          done();
+        })
+        .listen();
+    }); // should federate with provider and execute action
+    
+    it('should federate with provider and execute multiple actions', function(done) {
+      var actions = new Object();
+      actions.dispatch = sinon.stub().yieldsAsync(null);
+      var idp = new Object();
+      var idpFactory = new Object();
+      idpFactory.create = sinon.stub().resolves(idp);
+      var authenticateSpy = sinon.spy(authenticate);
+      var store = new Object();
+      store.get = sinon.stub().yieldsAsync(null, {
+        location: 'https://www.example.com/oauth/callback',
+        provider: 'http://sp.example.com',
+        action: [ 'login', 'authorize' ]
+      });
+      store.destroy = sinon.stub().yieldsAsync();
+      
+      
+      var handler = factory(actions, idpFactory, { authenticate: authenticateSpy }, store);
+    
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.connection = { encrypted: true };
+          req.method = 'GET';
+          req.url = '/oauth/callback';
+          req.headers.host = 'www.example.com';
+          req.params = { hostname: 'twitter.com' };
+          req.query = { oauth_token: 'XXXXXXXX' };
+          req.session = {};
+          req.session.state = {};
+          req.session.state['oauth_twitter.com_XXXXXXXX'] = { provider: 'http://sp.example.com' };
+        
+          res.resumeState = sinon.spy(function(cb) {
+            if (request.state.returnTo) {
+              return this.redirect(request.state.returnTo);
+            }
+          
+            process.nextTick(cb);
+          });
+        })
+        .finish(function() {
+          expect(store.get).to.be.calledOnceWith(this.req, 'oauth_twitter.com_XXXXXXXX');
+          expect(idpFactory.create).to.be.calledOnceWithExactly('http://sp.example.com', 'oauth');
+          expect(authenticateSpy).to.be.calledOnceWithExactly(idp, { assignProperty: 'federatedUser' });
+          expect(actions.dispatch).to.be.calledTwice;
+          expect(actions.dispatch.firstCall).to.be.calledWith('login');
+          expect(actions.dispatch.secondCall).to.be.calledWith('authorize');
+          expect(store.destroy).to.be.calledOnceWith(this.req, 'oauth_twitter.com_XXXXXXXX');
+          
+          expect(this.statusCode).to.equal(302);
+          expect(this.getHeader('Location')).to.equal('/');
+          
+          done();
+        })
+        .listen();
+    }); // should federate with provider and execute multiple actions
+    
   }); // handler
   
 });
