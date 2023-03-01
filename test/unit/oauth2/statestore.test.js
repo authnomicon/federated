384ab62c0c1c8fb6588baccf20acc7f8543f3056
Jared Hanson <jaredhanson@gmail.com>
2023-03-01 15:50:47 -0800

    Add tests.

--- test/unit/oauth2/statestore.test.js
+++ test/unit/oauth2/statestore.test.js
@@ -11,6 +11,27 @@ describe('oauth2/StateStore', function() {
   
   describe('#store', function() {
     
+    it('should push state and yield handle', function(done) {
+      var req = new Object();
+      req.pushState = sinon.stub().yieldsAsync(null, 'xyz');
+      
+      var state = {};
+      var meta = {
+        authorizationURL: 'https://server.example.com/authorize',
+        tokenURL: 'https://server.example.com/token',
+        clientID: 's6BhdRkqt3',
+        callbackURL: 'https://client.example.com/cb'
+      };
+      
+      store.store(req, state, meta, function(err, handle) {
+        if (err) { return done(err); }
+        
+        expect(req.pushState).to.have.been.calledOnceWith({}, 'https://client.example.com/cb');
+        expect(handle).to.equal('xyz');
+        done();
+      });
+    }); // should push state and yield handle
+    
     it('should push state with provider and yield handle', function(done) {
       var req = new Object();
       req.pushState = sinon.stub().yieldsAsync(null, 'xyz');
@@ -38,7 +59,7 @@ describe('oauth2/StateStore', function() {
       var req = new Object();
       req.pushState = sinon.stub().yieldsAsync(new Error('something went wrong'));
     
-      var state = { provider: 'https://server.example.com' };
+      var state = {};
       var meta = {
         authorizationURL: 'https://server.example.com/authorize',
         tokenURL: 'https://server.example.com/token',
@@ -47,9 +68,7 @@ describe('oauth2/StateStore', function() {
       };
       
       store.store(req, state, meta, function(err, handle) {
-        expect(req.pushState).to.have.been.calledOnceWith({
-          provider: 'https://server.example.com'
-        }, 'https://client.example.com/cb');
+        expect(req.pushState).to.have.been.calledOnceWith({}, 'https://client.example.com/cb');
         expect(err).to.be.an.instanceOf(Error);
         expect(err.message).to.equal('something went wrong');
         expect(handle).to.be.undefined;
