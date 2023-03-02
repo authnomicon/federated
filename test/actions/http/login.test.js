25da9cad1b94dd517237809c88ee4e8cc6714e04
Jared Hanson <jaredhanson@gmail.com>
2023-03-02 13:04:47 -0800

    Add tests.

--- test/actions/http/login.test.js
+++ test/actions/http/login.test.js
@@ -20,13 +20,62 @@ describe('actions/http/login', function() {
       .request(function(req, res) {
         req.login = sinon.stub().yieldsAsync(null);
         
-        req.federatedUser = { id: '248289761001' };
+        req.federatedUser = {
+          id: '248289761001',
+          displayName: 'Jane Doe'
+        };
       })
       .next(function(err, req, res) {
-        expect(req.login).to.have.been.calledOnceWith({ id: '248289761001' });
+        expect(req.login).to.have.been.calledOnceWith({
+          id: '248289761001',
+          displayName: 'Jane Doe'
+        });
         done();
       })
       .listen();
   }); // should login when directory is externalized
+  
+  it('should provision user', function(done) {
+    var mapper = new Object();
+    mapper.find = sinon.stub().yieldsAsync(null);
+    mapper.create = sinon.stub().yieldsAsync(null);
+    var directory = new Object();
+    directory.create = sinon.stub().yieldsAsync(null, {
+      id: '703887',
+      displayName: 'Jane Doe'
+    });
+    
+    var handler = factory(mapper, directory);
+  
+    chai.express.use(handler)
+      .request(function(req, res) {
+        req.login = sinon.stub().yieldsAsync(null);
+        
+        req.federatedUser = {
+          id: '248289761001',
+          displayName: 'Jane Doe'
+        };
+        req.state = {
+          provider: 'https://server.example.com'
+        };
+      })
+      .next(function(err, req, res) {
+        expect(mapper.find).to.have.been.calledOnceWith('248289761001', 'https://server.example.com');
+        expect(directory.create).to.have.been.calledOnceWith({
+          id: '248289761001',
+          displayName: 'Jane Doe'
+        });
+        expect(mapper.create).to.have.been.calledOnceWith('248289761001', 'https://server.example.com', {
+          id: '703887',
+          displayName: 'Jane Doe'
+        });
+        expect(req.login).to.have.been.calledOnceWith({
+          id: '703887',
+          displayName: 'Jane Doe'
+        });
+        done();
+      })
+      .listen();
+  }); // should provision user
   
 });
