e1284545e8c5e4bf4aa187932a0dd7bc83ea5bfb
Jared Hanson <jaredhanson@gmail.com>
2023-03-02 06:13:22 -0800

    Add tests.

--- test/unit/openidconnect/statestore.test.js
+++ test/unit/openidconnect/statestore.test.js
@@ -16,6 +16,31 @@ describe('openidconnect/StateStore', function() {
       req.pushState = sinon.stub().yieldsAsync(null, 'af0ifjsldkj');
     
       var ctx = {};
+      var state = {};
+      var meta = {
+        issuer: 'https://server.example.com',
+        authorizationURL: 'https://server.example.com/authorize',
+        tokenURL: 'https://server.example.com/token',
+        clientID: 's6BhdRkqt3',
+        callbackURL: 'https://client.example.com/cb'
+      };
+    
+      store.store(req, ctx, state, meta, function(err, handle) {
+        if (err) { return done(err); }
+      
+        expect(req.pushState).to.have.been.calledOnceWith({
+          protocol: 'openidconnect'
+        }, 'https://client.example.com/cb');
+        expect(handle).to.equal('af0ifjsldkj');
+        done();
+      });
+    }); // should push state and yield handle
+    
+    it('should push state with provider and yield handle', function(done) {
+      var req = new Object();
+      req.pushState = sinon.stub().yieldsAsync(null, 'af0ifjsldkj');
+    
+      var ctx = {};
       var state = { provider: 'https://server.example.com' };
       var meta = {
         issuer: 'https://server.example.com',
@@ -23,7 +48,7 @@ describe('openidconnect/StateStore', function() {
         tokenURL: 'https://server.example.com/token',
         clientID: 's6BhdRkqt3',
         callbackURL: 'https://client.example.com/cb'
-      }
+      };
     
       store.store(req, ctx, state, meta, function(err, handle) {
         if (err) { return done(err); }
@@ -32,11 +57,10 @@ describe('openidconnect/StateStore', function() {
           provider: 'https://server.example.com',
           protocol: 'openidconnect'
         }, 'https://client.example.com/cb');
-      
         expect(handle).to.equal('af0ifjsldkj');
         done();
       });
-    }); // should push state and yield handle
+    }); // should push state with provider and yield handle
     
     it('should push state with nonce and yield handle', function(done) {
       var req = new Object();
