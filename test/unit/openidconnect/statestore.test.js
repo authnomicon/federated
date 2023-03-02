4a78c69fb24a41d1aa9a91afc87db067412d8f8d
Jared Hanson <jaredhanson@gmail.com>
2023-03-02 06:53:30 -0800

    Add support for maxAge and issued to OIDC state store.

--- test/unit/openidconnect/statestore.test.js
+++ test/unit/openidconnect/statestore.test.js
@@ -89,6 +89,34 @@ describe('openidconnect/StateStore', function() {
       });
     }); // should push state with nonce and yield handle
     
+    it('should push state with max age and yield handle', function(done) {
+      var req = new Object();
+      req.pushState = sinon.stub().yieldsAsync(null, 'af0ifjsldkj');
+    
+      var ctx = { maxAge: 86400, issued: new Date('2011-07-21T20:42:50.000Z') };
+      var state = { provider: 'https://server.example.com' };
+      var meta = {
+        issuer: 'https://server.example.com',
+        authorizationURL: 'https://server.example.com/authorize',
+        tokenURL: 'https://server.example.com/token',
+        clientID: 's6BhdRkqt3',
+        callbackURL: 'https://client.example.com/cb'
+      };
+    
+      store.store(req, ctx, state, meta, function(err, handle) {
+        if (err) { return done(err); }
+      
+        expect(req.pushState).to.have.been.calledOnceWith({
+          provider: 'https://server.example.com',
+          protocol: 'openidconnect',
+          maxAge: 86400,
+          issued: new Date('2011-07-21T20:42:50.000Z')
+        }, 'https://client.example.com/cb');
+        expect(handle).to.equal('af0ifjsldkj');
+        done();
+      });
+    }); // should push state with max age and yield handle
+    
     it('should yeild error when pushing state fails', function(done) {
       var req = new Object();
       req.pushState = sinon.stub().yieldsAsync(new Error('something went wrong'));
@@ -149,6 +177,72 @@ describe('openidconnect/StateStore', function() {
         done();
       });
     }); // should verify state with nonce
+    
+    it('should verify state with max age', function(done) {
+      var req = new Object();
+      req.state = new Object();
+      req.state.complete = sinon.spy();
+      req.state.nonce = 'n-0S6_WzA2Mj';
+      req.state.maxAge = 86400;
+      req.state.issued = new Date('2011-07-21T20:42:50.000Z');
+      
+      store.verify(req, 'af0ifjsldkj', function(err, ctx) {
+        if (err) { return done(err); }
+        
+        expect(req.state.complete).to.have.been.calledOnce;
+        
+        expect(ctx).to.deep.equal({
+          nonce: 'n-0S6_WzA2Mj',
+          maxAge: 86400,
+          issued: new Date('2011-07-21T20:42:50.000Z')
+        });
+        done();
+      });
+    }); // should verify state with max age
+    
+    it('should verify state with max age', function(done) {
+      var req = new Object();
+      req.state = new Object();
+      req.state.complete = sinon.spy();
+      req.state.nonce = 'n-0S6_WzA2Mj';
+      req.state.maxAge = 86400;
+      req.state.issued = new Date('2011-07-21T20:42:50.000Z');
+      
+      store.verify(req, 'af0ifjsldkj', function(err, ctx) {
+        if (err) { return done(err); }
+        
+        expect(req.state.complete).to.have.been.calledOnce;
+        
+        expect(ctx).to.deep.equal({
+          nonce: 'n-0S6_WzA2Mj',
+          maxAge: 86400,
+          issued: new Date('2011-07-21T20:42:50.000Z')
+        });
+        done();
+      });
+    }); // should verify state with max age
+    
+    it('should verify state with max age where issued is a string', function(done) {
+      var req = new Object();
+      req.state = new Object();
+      req.state.complete = sinon.spy();
+      req.state.nonce = 'n-0S6_WzA2Mj';
+      req.state.maxAge = 86400;
+      req.state.issued = '2011-07-21T20:42:50.000Z';
+      
+      store.verify(req, 'af0ifjsldkj', function(err, ctx) {
+        if (err) { return done(err); }
+        
+        expect(req.state.complete).to.have.been.calledOnce;
+        
+        expect(ctx).to.deep.equal({
+          nonce: 'n-0S6_WzA2Mj',
+          maxAge: 86400,
+          issued: new Date('2011-07-21T20:42:50.000Z')
+        });
+        done();
+      });
+    }); // should verify state with max age where issued is a string
     
     it('should yield error when state middleware is not in use', function(done) {
       var req = new Object();
