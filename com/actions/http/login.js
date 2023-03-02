c409e42a85b4ef257c22d8ef53e4e7ec82ad0942
Jared Hanson <jaredhanson@gmail.com>
2023-03-02 13:55:15 -0800

    Clean up.

--- com/actions/http/login.js
+++ com/actions/http/login.js
@@ -1,8 +1,6 @@
 exports = module.exports = function(idMapper, directory) {
   
-  // TODO: Rename "Federated ID server" to iss-sub service, following.
   // https://www.ietf.org/archive/id/draft-ietf-secevent-subject-identifiers-14.html#name-issuer-and-subject-identifi
-  
   
   function login(req, res, next) {
     if (!idMapper) {
@@ -11,38 +9,37 @@ exports = module.exports = function(idMapper, directory) {
         if (err) { return next(err); }
         return next();
       });
-      return;
-    }
-    
-    idMapper.get(req.federatedUser, req.state.provider, function(err, user) {
-      if (err) { return next(err); }
+    } else {
+      idMapper.get(req.federatedUser, req.state.provider, function(err, user) {
+        if (err) { return next(err); }
       
-      if (!user) {
-        // JIT the user
-        directory.create(req.federatedUser, function(err, user) {
-          if (err) { return next(err); }
-          
-          idMapper.set(req.federatedUser, req.state.provider, user, function(err) {
+        if (!user) {
+          // JIT the user
+          directory.create(req.federatedUser, function(err, user) {
             if (err) { return next(err); }
+          
+            idMapper.set(req.federatedUser, req.state.provider, user, function(err) {
+              if (err) { return next(err); }
             
+              req.login(user, function(err) {
+                if (err) { return next(err); }
+                return next();
+              });
+            });
+          });
+        } else {
+          // Load the user, already JIT'ed
+          directory.read(user.id, function(err, user) {
+            if (err) { return next(err); }
+          
             req.login(user, function(err) {
               if (err) { return next(err); }
               return next();
             });
           });
-        });
-      } else {
-        // Load the user, already JIT'ed
-        directory.read(user.id, function(err, user) {
-          if (err) { return next(err); }
-          
-          req.login(user, function(err) {
-            if (err) { return next(err); }
-            return next();
-          });
-        });
-      }
-    });
+        }
+      });
+    }
   }
   
   
