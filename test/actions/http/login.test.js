d24ca68e464aeb6558f2d5034125428d81b067db
Jared Hanson <jaredhanson@gmail.com>
2023-08-09 10:07:13 -0700

    Add tests.

--- test/actions/http/login.test.js
+++ test/actions/http/login.test.js
@@ -280,6 +280,45 @@ describe('actions/http/login', function() {
         .listen();
     }); // should next with error when session initiation fails after provisioning
     
+    it('should next with error when account fails to be found in directory', function(done) {
+      var idStore = new Object();
+      idStore.find = sinon.stub().yieldsAsync(null, {
+        id: '703887'
+      });
+      idStore.add = sinon.spy()
+      var directory = new Object();
+      directory.create = sinon.spy();
+      directory.read = sinon.stub().yieldsAsync(new Error('something went wrong'));
+    
+      var handler = factory(idStore, directory);
+  
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.login = sinon.stub().yieldsAsync(null);
+        
+          req.federatedUser = {
+            id: '248289761001',
+            displayName: 'Jane Doe'
+          };
+          req.state = {
+            provider: 'https://server.example.com'
+          };
+        })
+        .next(function(err, req, res) {
+          expect(err).to.be.an.instanceOf(Error);
+          expect(err.message).to.equal('something went wrong');
+          
+          expect(idStore.find).to.have.been.called;
+          expect(idStore.add).to.not.have.been.called;
+          expect(directory.create).to.not.have.been.called;
+          expect(directory.read).to.have.been.called;
+          expect(req.login).to.not.have.been.called;
+          
+          done();
+        })
+        .listen();
+    }); // should next with error when account fails to be found in directory
+    
     it('should login locally when identifier store is not available', function(done) {
       var handler = factory();
   
