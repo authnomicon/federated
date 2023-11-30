e4640fcb21263bb36f08a8b4307d4d353cd69496
Jared Hanson <jaredhanson@gmail.com>
2023-11-30 09:16:36 -0800

    Add documentation.

--- com/actions/http/authorize.js
+++ com/actions/http/authorize.js
@@ -1,10 +1,28 @@
 /**
  * Create authorize action.
  *
+ * Returns an action that authorizes the application to operate on behalf of the
+ * user.
+ *
+ * This action is executed upon federating with a external service.  In this
+ * case, the external service is typically an authorization server implementing
+ * a delegated authorization protocol such as OAuth 2.0.  The authorization
+ * server issues the application delegation-specific credentials (typically
+ * referred to as an access token), which allow the application to access the
+ * external service on behalf of the user.
+ *
+ * This action stores the token issued by the service provider in a delegated
+ * credential store.  The application is expected to load the credential from
+ * the store when it accesses the service.
  */
 exports = module.exports = function(store, authenticator) {
 
   function stashAuthInfo(req, res, next) {
+    // Stash the info from the federated authorization response.  This is done
+    // because the local user who initiated authorization will be authenticated
+    // by authenticating the session.  This authentication will overwrite
+    // `req.authInfo`, and a reference to it needs to be maintained for the
+    // duration of request handling.
     req.federatedAuthInfo = req.authInfo;
     next();
   }
@@ -17,6 +35,7 @@ exports = module.exports = function(store, authenticator) {
     });
   }
   
+  
   // TODO: Should probably authenticate completely based on 'state' here, and remove stashAuthInfo
   return [
     stashAuthInfo,
@@ -26,6 +45,7 @@ exports = module.exports = function(store, authenticator) {
   ];
 };
 
+// Module annotations.
 exports['@require'] = [
   'module:@authnomicon/federated.DelegatedCredentialStore',
   'module:passport.Authenticator'
